331521b42b7c5fda88cbac0ee0119b2b93768701
kingmades <mades.almeida@outlook.com>
2024-12-28 16:12:38 -0300

    fix read file

--- TextEditor/Program.cs
+++ TextEditor/Program.cs
@@ -31,7 +31,7 @@ class Program
 		Console.WriteLine("Qual caminho do arquivo?");
 		string path = Console.ReadLine();
 
-		using (var file = new StreamReader(path))
+		using (var file = new StreamReader(path!))
 		{
 			string text = file.ReadToEnd();
 			Console.WriteLine(text);
